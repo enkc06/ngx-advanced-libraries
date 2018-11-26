c8cb43828f0f9ccde5404edb1663fd8b3d0dd735
Naren Edpuganti <naren.edpuganti@trunow.com>
2018-11-26 11:49:40 -0500

    fix props

--- projects/ngx-advanced-utils/src/lib/collection.service.ts
+++ projects/ngx-advanced-utils/src/lib/collection.service.ts
@@ -7,72 +7,104 @@ export class CollectionService {
   constructor() {}
 
   // filter with two array of objects by prop
-  filterTwoArrayOfObjectsByProp(array1, array2, prop) {
+  filterTwoArrayOfObjectsByProp(
+    array1: any[],
+    array2: any[],
+    prop1: string,
+    prop2: string
+  ) {
     return array1.filter(ele1 => {
-      return array2.some(ele2 => ele1[prop] === ele2[prop]);
+      return array2.some(ele2 => ele1[prop1] === ele2[prop2]);
     });
   }
 
   // uniq filter with two array of objects by prop
-  UniqFilterTwoArrayOfObjectsByProp(array1, array2, prop) {
+  UniqFilterTwoArrayOfObjectsByProp(
+    array1: any[],
+    array2: any[],
+    prop1: string,
+    prop2: string
+  ) {
     return array1.filter(ele1 => {
-      return !array2.some(ele2 => ele1[prop] === ele2[prop]);
+      return !array2.some(ele2 => ele1[prop1] === ele2[prop2]);
     });
   }
 
   // uniq filter with two array of objects by prop and removingDuplicates
-  uniqFilterTwoArrayOfObjectsByPropAndRemoveDuplicates(array1, array2, prop) {
-    const newArr = array1.filter(ele1 => {
-      return array2.some(ele2 => ele1[prop] === ele2[prop]);
-    });
-
-    return newArr.filter((obj, pos, arr) => {
-      const arrayObj = arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]);
-      return arrayObj === pos;
-    });
+  uniqFilterTwoArrayOfObjectsByPropAndRemoveDuplicates(
+    array1: any[],
+    array2: any[],
+    prop1: string,
+    prop2: string
+  ) {
+    return array1
+      .filter(ele1 => {
+        return array2.some(ele2 => ele1[prop1] === ele2[prop2]);
+      })
+      .filter((obj, pos, arr) => {
+        return arr.map(mapObj => mapObj[prop1]).indexOf(obj[prop2]) === pos;
+      });
   }
 
   // notuniq filter with two array of objects by prop and removingDuplicates
   notUniqFilterTwoArrayOfObjectsByPropAndRemoveDuplicates(
-    array1,
-    array2,
-    prop
+    array1: any[],
+    array2: any[],
+    prop1: string,
+    prop2: string
   ) {
-    const newArr = array1.filter(ele1 => {
-      return array2.some(ele2 => ele1[prop] === ele2[prop]);
-    });
-
-    return newArr.filter((obj, pos, arr) => {
-      const arrayObj = arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]);
-      return arrayObj !== pos;
-    });
+    return array1
+      .filter(ele1 => {
+        return array2.some(ele2 => ele1[prop1] === ele2[prop2]);
+      })
+      .filter((obj, pos, arr) => {
+        return arr.map(mapObj => mapObj[prop1]).indexOf(obj[prop2]) !== pos;
+      });
   }
 
   // filter array of objects with array with array by prop
-  filterArrayOfObjectsArrayWithArrayByProp(array1, array2, prop) {
+  filterArrayOfObjectsArrayWithArrayByProp(
+    array1: any[],
+    array2: any[],
+    prop: string
+  ) {
     return array1.filter(ele1 => {
       return array2.some(ele2 => ele1[prop].includes(ele2));
     });
   }
 
   // uniq filter array of objects array with array by prop
-  uniqFilterArrayOfObjectsArrayWithArrayByProp(array1, array2, prop) {
+  uniqFilterArrayOfObjectsArrayWithArrayByProp(
+    array1: any[],
+    array2: any[],
+    prop: string
+  ) {
     return array1.filter(ele1 => {
       return !array2.some(ele2 => ele1[prop].includes(ele2));
     });
   }
 
   // filter array of objects array with array of objects by prop
-  filterArrayOfObjectsArrayWithArrayOfObjectsByProp(array1, array2, prop) {
+  filterArrayOfObjectsArrayWithArrayOfObjectsByProp(
+    array1: any[],
+    array2: any[],
+    prop1: string,
+    prop2: string
+  ) {
     return array1.filter(ele1 => {
-      return array2.some(ele2 => ele1[prop].includes(ele2[prop]));
+      return array2.some(ele2 => ele1[prop1].includes(ele2[prop2]));
     });
   }
 
   // uniq filter array of objects array with array of objects by prop
-  uniqFilterArrayOfObjectsArrayWithArrayOfObjectsByProp(array1, array2, prop) {
+  uniqFilterArrayOfObjectsArrayWithArrayOfObjectsByProp(
+    array1: any[],
+    array2: any[],
+    prop1: string,
+    prop2: string
+  ) {
     return array1.filter(ele1 => {
-      return !array2.some(ele2 => ele1[prop].includes(ele2[prop]));
+      return !array2.some(ele2 => ele1[prop1].includes(ele2[prop2]));
     });
   }
 }
